2b17917112ce1c83219c93f3071cdfa18faa9713
jobvillanca <76563339+jobvillanca@users.noreply.github.com>
2021-03-03 17:01:03 -0500

    deleted the features folder

--- Test Page/results.js
+++
@@ -1,7 +0,0 @@
-const resultsList = document.getElementById("results");
-      console.log(resultsList);
-
-     /* new URLSearchParams(window.location.search).forEach((value, name) => {
-        resultsList.append(`${name}: ${value}`);
-        resultsList.append(document.createElement("br"));
-      });
