404b44ddf32465b4e10a8ff7efcd8b841f91d6ee
jobvillanca <76563339+jobvillanca@users.noreply.github.com>
2021-03-17 04:26:10 -0400

    adding small edits, links and portfolio's

---
+++ The Eye/eyes.js
@@ -0,0 +1,35 @@
+const balls = document.getElementsByClassName("ball");
+document.onmousemove = (event) => {
+  const x = (event.clientX * 100) / window.innerWidth + "%";
+  const y = (event.clientY * 100) / window.innerHeight + "%";
+
+  for (let i = 0; i < 2; i++) {
+    balls[i].style.left = x;
+    balls[i].style.top = y;
+    balls[i].transform = "translate(-" + x + ",-" + y + ")";
+  }
+};
+
+// let teeth = URL("./teeth.png");
+// function x() {
+//   setTimeout(() => {
+//     document.InerHTML.(teeth);
+//   }, 2000);
+// }
+// x();
+
+var img = new Image();
+var div = document.getElementById("smile");
+img.src = "../assets/img/portfolioNew/teeth.png";
+
+function x() {
+  div.appendChild(img);
+}
+
+// setTimeout(() => {
+
+// }, 2000);
+
+setTimeout(() => {
+  x();
+}, 2500);
